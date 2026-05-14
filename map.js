6c9f818f2c9a468a15cc0aa739288067dfa20c6d
Joshuaamm7 <joshhw921222@gmail.com>
2026-05-14 11:03:38 -0700

    step1

--- map.js
+++ map.js
@@ -3,3 +3,16 @@ import mapboxgl from 'https://cdn.jsdelivr.net/npm/mapbox-gl@2.15.0/+esm';
 
 // Check that Mapbox GL JS is loaded
 console.log('Mapbox GL JS Loaded:', mapboxgl);
+
+// Set your Mapbox access token here
+mapboxgl.accessToken = 'pk.eyJ1Ijoiam9zaGh1YTkyMTIiLCJhIjoiY21wNXNmbHMzMTM2NzJ5bzRqcHo0anBodCJ9.cpABO1VuMCx3kfvl_gRfjQ';
+
+// Initialize the map
+const map = new mapboxgl.Map({
+  container: 'map',
+  style: 'mapbox://styles/mapbox/streets-v12',
+  center: [-71.09415, 42.36027],
+  zoom: 12,
+  minZoom: 5,
+  maxZoom: 18,
+});

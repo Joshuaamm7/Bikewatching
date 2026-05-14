d0afd9831ca8e68a3994c4d64f3c561d69ce6c0f
Joshuaamm7 <joshhw921222@gmail.com>
2026-05-14 11:31:44 -0700

    s3

--- map.js
+++ map.js
@@ -36,7 +36,7 @@ const bikeLaneStyle = {
 
 // Helper function to convert station longitude/latitude into screen coordinates
 function getCoords(station) {
-  const point = new mapboxgl.LngLat(+station.Long, +station.Lat);
+  const point = new mapboxgl.LngLat(+station.lon, +station.lat);
   const { x, y } = map.project(point);
   return { cx: x, cy: y };
 }
